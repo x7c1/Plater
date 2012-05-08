27e5521462b86813bc359c3d6c198d39c2ad8635
x7c1 <jmtaro+x7c1@gmail.com>
2012-05-09 02:54:20 +0900

    add higher-order functions

--- src/main/collection/iterator/Xc_Iterator.php
+++ src/main/collection/iterator/Xc_Iterator.php
@@ -1,9 +1,110 @@
-<?php
+<?
 namespace x7c1\plater\collection\iterator;
 
-class Xc_Iterator {
-    public function __construct(){
-        $this->sample = 1;
+trait Xc_IteratorBase {
+    public function map($callback){
+        return new Xc_MappedIterator($this, $callback);
     }
+    public function filter($callback){
+        return new Xc_FilteredIterator($this, $callback);
+    }
+}
+
+class Xc_Iterator implements \Iterator{
+    use Xc_IteratorBase;
+
+    protected $position;
+    protected $underlying;
+
+    public function __construct($array){
+        $this->position = 0;
+        $this->underlying = $array;
+    }
+
+    public function current(){
+        return $this->underlying[$this->position];
+    }
+    public function key(){
+        return $this->position;
+    }
+    public function next(){
+        ++$this->position;
+    }
+    public function rewind(){
+        $this->position = 0;
+    }
+    public function valid(){
+        $count = count($this->underlying);
+        return $this->position < $count;
+    }
+}
+
+trait Xc_IteratorDelegatee {
+    public function current(){
+        return $this->underlying->current();
+    }
+    public function key(){
+        return $this->underlying->key();
+    }
+    public function next(){
+        $this->underlying->next();
+    }
+    public function rewind(){
+        $this->underlying->rewind();
+    }
+    public function valid(){
+        return $this->underlying->valid();
+    }
+}
+
+class Xc_MappedIterator implements \Iterator{
+    use Xc_IteratorBase;
+    use Xc_IteratorDelegatee;
+
+    protected $underlying;
+    protected $callback;
+
+    public function __construct(\Iterator $iterator, $callback){
+        $this->underlying = $iterator;
+        $this->callback = $callback;
+    }
+
+    public function current(){
+        $current = $this->underlying->current();
+        return call_user_func($this->callback, $current);
+    }
+}
+
+class Xc_FilteredIterator implements \Iterator{
+    use Xc_IteratorBase;
+    use Xc_IteratorDelegatee;
+
+    protected $underlying;
+    protected $callback;
+
+    public function __construct(\Iterator $iterator, $callback){
+        $this->underlying = $iterator;
+        $this->callback = $callback;
+    }
+
+    public function valid(){
+        list($is_target, $is_valid) = $this->inspect_next();
+        while(!$is_target && $is_valid){
+            $this->underlying->next();
+            list($is_target, $is_valid) = $this->inspect_next();
+        }
+        return $is_valid;
+    }
+
+    protected function inspect_next(){
+        $is_valid = $this->underlying->valid();
+        if ($is_valid){
+            $is_target = call_user_func($this->callback, $this->underlying->current());
+        } else {
+            $is_target = false;
+        }
+        return array($is_target, $is_valid);
+    }
+
 }
 
--- src/test/collection/iterator/Xc_IteratorTest.php
+++ src/test/collection/iterator/Xc_IteratorTest.php
@@ -1,14 +1,60 @@
 <?
-//namespace x7c1\plater\collection\iterator;
-
 use x7c1\plater\collection\iterator\Xc_Iterator;
 
 class Xc_IteratorTest extends \PHPUnit_Framework_TestCase{
 
-    public function test_sample(){
-        $iter = new Xc_Iterator;
-        $this->assertSame(1, $iter->sample);
-        $this->assertNotSame(2, $iter->sample);
+    public function test_empty(){
+        $iter = new Xc_Iterator([]);
+        $this->assertSame([], $this->iter_to_array($iter));
+    }
+
+    public function test_map(){
+        $iter = new Xc_Iterator([2, 3, 4]);
+
+        $mul = function($x){
+            return $x * 2;
+        };
+        $mul_2 = $this->iter_to_array($iter->map($mul));
+        $this->assertSame([4, 6, 8], $mul_2);
+
+        $mul_4 = $this->iter_to_array($iter->map($mul)->map($mul));
+        $this->assertSame([8, 12, 16], $mul_4);
+
+        $incr = function($x){
+            return $x + 1;
+        };
+        $incr_then_mul = $iter->map($incr)->map($mul);
+        $this->assertSame([6, 8, 10], $this->iter_to_array($incr_then_mul));
+
+        $mul_then_incr = $iter->map($mul)->map($incr);
+        $this->assertSame([5, 7, 9], $this->iter_to_array($mul_then_incr));
+    }
+
+    public function test_filter(){
+        $iter = new Xc_Iterator([2, 3, 4, 5]);
+
+        $is_greater = function($x){
+            return $x > 2;
+        };
+        $x = $iter->filter($is_greater);
+        $this->assertSame([3, 4, 5], $this->iter_to_array($x));
+
+        $x2 = $x->map(function($i){ return 2 * $i; });
+        $this->assertSame([6, 8, 10], $this->iter_to_array($x2));
+
+        $x3 = $x2->filter(function($i){ return $i > 7; });
+        $this->assertSame([8, 10], $this->iter_to_array($x3));
+
+        $x4 = $x3->map(function($i){ return ++$i; });
+        $this->assertSame([9, 11], $this->iter_to_array($x4));
+    }
+
+    private function iter_to_array($iter){
+        $array = [];
+        foreach($iter as $i => $x){
+            $array[] = $x;
+        }
+        return $array;
     }
 
 }
